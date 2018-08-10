133d0a0e5e5326bfd2921468accde9bb0ad480f5
hahahey <15094025379@163.com>
2018-08-10 15:32:12 +0800

    修改了master，增加了222

--- Kafka/src/main/java/com/lzk/Test.java
+++ Kafka/src/main/java/com/lzk/Test.java
@@ -2,6 +2,6 @@ package com.lzk;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("first git example dev modify");
+        System.out.println("first git example dev modify 222");
     }
 }
