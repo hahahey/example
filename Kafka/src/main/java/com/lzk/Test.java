797d3ed5bf2a73263e8b5b669e3e50e51d2ab552
hahahey <15094025379@163.com>
2018-08-11 16:27:46 +0800

    delete

--- Kafka/src/main/java/com/lzk/Test.java
+++
@@ -1,8 +0,0 @@
-package com.lzk;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("first git example dev modify 333");
-        System.out.println("first git example dev modify 222");
-    }
-}
